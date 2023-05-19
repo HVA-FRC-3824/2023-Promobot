289bc8e684bdfc0b9d1bb4f60f1b5a4f366978dc
bogogion <45zzzpvp@gmail.com>
2023-05-19 10:58:28 -0500

    removed some comments

--- 2023-Promobot/src/main/java/frc/robot/Robot.java
+++ 2023-Promobot/src/main/java/frc/robot/Robot.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
@@ -9,10 +5,6 @@ import edu.wpi.first.wpilibj.TimedRobot;
 
 import frc.robot.subsystems.*;
 
-/**
- * This is a demo program showing the use of the DifferentialDrive class, specifically it contains
- * the code necessary to operate a robot with tank drive.
- */
 public class Robot extends TimedRobot {
   private Joystick left_joystick;
   private Joystick right_joystick;
