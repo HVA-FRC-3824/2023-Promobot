4cbbf7c7e78326d426dc3eecec0ac3fdf7ee4363
bogogion <45zzzpvp@gmail.com>
2023-05-24 18:27:47 -0500

    pneumatics

--- 2023-Promobot/src/main/java/frc/robot/Robot.java
+++ 2023-Promobot/src/main/java/frc/robot/Robot.java
@@ -2,25 +2,31 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.subsystems.*;
 
 public class Robot extends TimedRobot {
   private Joystick left_joystick;
   private Joystick right_joystick;
+  private JoystickButton compressor_button;
 
   public WestCoast WestCoastDrive;
+  public Pneumatics PneumaticsControl;
 
   @Override
   public void robotInit() {
     WestCoastDrive = new WestCoast();
+    PneumaticsControl = new Pneumatics(0,0);
 
     left_joystick  = new Joystick(Constants.LEFT_JOYSTICK_ID);
     right_joystick = new Joystick(Constants.RIGHT_JOYSTICK_ID);
+    compressor_button = new JoystickButton(left_joystick, 0);
   }
 
   @Override
   public void teleopPeriodic() {
+    // TODO: Fix right_joystick!
     WestCoastDrive.Drive(-left_joystick.getY() * Constants.INPUT_MULTIPLIER_FORWARD, -right_joystick.getX() * Constants.INPUT_MULTIPLIER_SIDEWAYS);
+    
   }
 }
---
+++ 2023-Promobot/src/main/java/frc/robot/subsystems/Pneumatics.java
@@ -0,0 +1,42 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import static edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Pneumatics extends SubsystemBase {
+    private Compressor COMPRESSOR;
+    private DoubleSolenoid RELEASE_SOLENOID;
+
+    // Init
+    public Pneumatics(int forward_channel, int reverse_channel)
+    {
+        // ASSUMES THAT PCM CAN ID IS 0!
+        COMPRESSOR = new Compressor(PneumaticsModuleType.REVPH);
+        RELEASE_SOLENOID = new DoubleSolenoid(PneumaticsModuleType.REVPH, forward_channel, reverse_channel);
+    }
+
+    public void Void_Air()
+    {
+        // Releases both solenoids TODO: figure out if they need to be opened or closed (solenoid is configured weird)
+        RELEASE_SOLENOID.set(Value.kForward);
+        RELEASE_SOLENOID.set(Value.kReverse);
+    }
+
+    public void Close_Solenoid()
+    {
+        RELEASE_SOLENOID.close();
+    }
+
+    public void Enable_Compressor(boolean e_or_d)
+    {
+        if(e_or_d)
+        {
+            COMPRESSOR.enableDigital();
+        } else {
+            COMPRESSOR.close();
+        }
+    }
+}
